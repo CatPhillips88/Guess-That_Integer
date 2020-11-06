51a17a972fddb1737b5a27d5b059241dffb0a652
Catherine <catherineep4189@gmail.com>
2020-11-06 21:56:19 +0000

    Nesting previous conditional if statement in a while loop

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import random
 
 guess = random.randint(1,10)
 
+attempted_guesses = 0
+
 num_guess = int(input('Pick a number... '))
 
-if num_guess == guess:
+while attempted_guesses < 5:
+   if num_guess == guess:
     print('You\'ve guessed correctly!')
-else:
+    attempted_guesses += 1
+    print(attempted_guesses)
+   else:
     print('Guess Again!')
+    attempted_guesses += 1
+    print(attempted_guesses)
