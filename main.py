b41496f4c383513d340992eb4f4e994495b9354f
Catherine <catherineep4189@gmail.com>
2020-11-07 15:01:28 +0000

    conditions set to give hints to players when entering an integer

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import random
 
 guess = random.randint(1,10)
 
-
-
-
 attempted_guesses = 0
 
 while attempted_guesses < 5:
    num_guess = int(input('Pick a number... '))
 
-   if num_guess == guess:
-    print('You\'ve guessed correctly!')
-    attempted_guesses += 1
-    print(attempted_guesses)
-   else:
-    print('Guess Again!')
-    attempted_guesses += 1
-    print(attempted_guesses)
+   if num_guess != guess and num_guess > guess:
+       print('GUESS AGAIN, GO LOWER!')
+       attempted_guesses += 1
+       print(attempted_guesses)
+   elif num_guess != guess and num_guess < guess:
+       print('GUESS AGAIN, GO HIGHER')
+       attempted_guesses += 1
+       print(attempted_guesses)
+   elif num_guess == guess:
+       print('YOU\'VE GUESSED CORRECTLY')
+       attempted_guesses += 1
+       print(attempted_guesses)
