6642d4e020561ab66dba3b16de2d251494dcbe84
Catherine <catherineep4189@gmail.com>
2020-11-07 15:21:30 +0000

    minor formatting

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ guess = random.randint(1,10)
 attempted_guesses = 0
 
 while attempted_guesses < 5:
-   num_guess = int(input('Pick a number... '))
+   num_guess = int(input('\nPick a number... '))
 
    if num_guess > 10 and num_guess != guess:
        print('SORRY, GUESS ONLY NUMBERS BETWEEN 1 AND 10!')
-   if num_guess != guess and num_guess > guess:
+   elif num_guess != guess and num_guess > guess:
        print('GUESS AGAIN, GO LOWER!')
        attempted_guesses += 1
-       print(attempted_guesses)
+       print(f'CURRENT GUESS: {attempted_guesses}')
    elif num_guess != guess and num_guess < guess:
        print('GUESS AGAIN, GO HIGHER')
        attempted_guesses += 1
-       print(attempted_guesses)
+       print(f'CURRENT GUESS: {attempted_guesses}')
    elif num_guess == guess:
        print('YOU\'VE GUESSED CORRECTLY')
        attempted_guesses += 1
-       print(attempted_guesses)
+       print(f'CURRENT GUESS: {attempted_guesses}')
